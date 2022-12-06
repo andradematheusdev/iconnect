29e5f1a807f9eee32ade7950c15e4721c8db1899
Heloriel <andradematheus.dev@gmail.com>
2022-12-06 14:34:52 -0300

    post photo grid

--- components/UserPostBlock/UserPostBlock.tsx
+++ components/UserPostBlock/UserPostBlock.tsx
@@ -1,8 +1,22 @@
 import Block from '../Block/Block';
 import UserAvatar from '../UserAvatar/UserAvatar';
 import styles from './UserPostBlock.module.scss';
+import {ReactPhotoCollage} from 'react-photo-collage';
 
 const UserPostBlock = () => {
+
+  const settings = {
+    width: '600px',
+    height: ['250px', '170px'],
+    layout: [2, 1],
+    photos: [
+      { source: '/images/user/cover/cover.jpg' },
+      { source: '/images/user/cover/cover.jpg' },
+      { source: '/images/user/cover/cover.jpg' },
+    ],
+    showNumOfRemainingPhotos: true
+  };
+ 
   return (
     <Block className={styles.wrapper}>
       <Block.Head className={styles.head}>
@@ -26,8 +40,13 @@ const UserPostBlock = () => {
             </div>
           </div>
       </Block.Head>
-      <Block.Body>
-        Body Test
+      <Block.Body className={styles.body}>
+        <div className={styles.posttext}>
+          When the bass drops, so do my problems.
+        </div>
+        <div className={styles.picturegrid}>
+          <ReactPhotoCollage {...settings} />
+        </div>
       </Block.Body>
     </Block>    
   )
