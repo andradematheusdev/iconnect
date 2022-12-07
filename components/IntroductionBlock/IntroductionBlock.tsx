842c167d466706a64b698b307eaac6e739f6afce
Heloriel <andradematheus.dev@gmail.com>
2022-12-07 13:46:01 -0300

    Introduction block component update

--- components/IntroductionBlock/IntroductionBlock.tsx
+++ components/IntroductionBlock/IntroductionBlock.tsx
@@ -1,20 +1,22 @@
 import { Basketball, Briefcase, DotsThree, House } from 'phosphor-react';
 import MenuHeading from '../MenuHeading/MenuHeading';
 import styles from './IntroductionBlock.module.scss';
+import Block from '../Block/Block';
+
 
 const IntroductionBlock = () => {
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.header}>
+    <Block className={styles.wrapper}>
+      <Block.Head className={styles.header}>
         <MenuHeading text='Introduction' size={16} />
         <DotsThree size={24} color={"#fff"} weight="bold" />
-      </div>
-      <div className={styles.body}>
-        <div className={styles.topic}><Briefcase size={22} /><span>Works at <b>Helorelabs</b></span></div>
+      </Block.Head>
+      <Block.Body className={styles.body}>
+        <div className={styles.topic}><Briefcase size={22} /><span>Works at <a href="#"><b>Helorelabs</b></a></span></div>
         <div className={styles.topic}><House size={22} /><span>Lives in <b>Magé, RJ</b></span></div>
         <div className={styles.topic}><Basketball size={22} /><span>Player name <b>Helore</b></span></div>
-      </div>
-    </div>
+      </Block.Body>
+    </Block>
   )
 }
 
