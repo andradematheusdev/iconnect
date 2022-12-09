487f0ecdf0b30277cfd1547e636f37b4cc7c954e
Heloriel <andradematheus.dev@gmail.com>
2022-12-09 10:39:30 -0300

    Topics icon update

--- components/Layout/LeftNavbar/LeftNavbar.tsx
+++ components/Layout/LeftNavbar/LeftNavbar.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { Calendar, FileText, HouseSimple, ImageSquare, MapPin, Megaphone } from 'phosphor-react';
+import { Calendar, FileText, HashStraight, HouseSimple, ImageSquare, MapPin, Megaphone } from 'phosphor-react';
 import Logo from '../../Logo';
 import MenuHeading from '../../MenuHeading/MenuHeading';
 import NavItem from '../../NavItem/NavItem';
@@ -17,21 +17,17 @@ const LeftNavbar = () => {
                 <MenuHeading text='MENU' />
               </li>
               <NavItem text='Home' link='#' icon={<HouseSimple size={22} color={"#fff"} />} />
-              <NavItem text='Latest News' link='#' icon={<Megaphone size={22} />} />
-              <NavItem text='Explore' link='#' icon={<MapPin size={22} />} />
-              <NavItem text='Files' link='#' icon={<FileText size={22} />} />
+              <NavItem text='News' link='#' icon={<Megaphone size={22} />} />
+              <NavItem text='Discovery' link='#' icon={<MapPin size={22} />} />
               <NavItem text='Galery' link='#' icon={<ImageSquare size={22} />} />
               <NavItem text='Events' link='#' icon={<Calendar size={22} />} />
               <li>
                 <MenuHeading text='YOUR FAVOURITE' />
               </li>
-              <NavItem text="Foresto" link="#" icon={"🌳"} />
-              <NavItem text="Birds" link="#" icon={"🐦"}/>
-              <NavItem text="Nature" link="#" icon={"🌴"} />
-              <NavItem text="Animals" link="#" icon={"🐢"} />
-              <NavItem text="Motobike" link="#" icon={"🚴‍♂️"} />
-              <NavItem text="Mojave" link="#" icon={"🌱"} />
-              <NavItem text="Chemistry" link="#" icon={"🧪"} />
+              <NavItem text="games" link="#" icon={<HashStraight size={22} />} />
+              <NavItem text="javascript" link="#" icon={<HashStraight size={22} />} />
+              <NavItem text="react" link="#" icon={<HashStraight size={22} />} />
+              <NavItem text="frontend" link="#" icon={<HashStraight size={22} />} />
             </ul>
           </nav>
           <footer>
